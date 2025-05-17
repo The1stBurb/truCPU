a3d5323be346ad56fcafd5e55dbde1cb19759f46
Etcetera_ <150498673+The1stBurb@users.noreply.github.com>
2025-05-17 08:43:16 -0600

    Update assemb.py
    potato

--- assemb.py
+++ assemb.py
@@ -8,38 +8,7 @@ ops={
     "int1":128,"int2":129,"int3":130,"int4":131,"int5":132,"int6":133,"int7":134,"int8":135,
     "hlt":255,
     # "ldac":"0000_0110","ldbc":"0000_0111","stac":"0000_1000","stbc":"0000_1001","nota":"0000_1010","notb":"0000_1011","shlb":"0000_1101","shla":"0000_1100","shrb":"0000_1111","shra":"0000_1110","incb":"0001_0001","inca":"0001_0000","decb":"0001_0011","deca":"0001_0010","popb":"0001_0101","popa":"0001_0100","psha":"0001_0110","pshb":"0001_0111","add":"0001_1000","sub":"0001_1001","and":"0001_1010","or":"0001_1011","xor":"0001_1100","cmp":"0001_1101","swap":"0001_1110","ldav":"0010_0100","ldbv":"0010_0101",# "jmp":"0011_0000","jmz":"0011_0010","jnz":"0011_0011","jmc":"0011_0100","jnc":"0011_0101","jme":"0011_0110","jne":"0011_0111","jma":"0011_1000","jna":"0011_1001","call":"0011_1010","ret":"0011_1111",# "incsp":"0010_0000","decsp":"0010_0001","push":"0010_0010","pop":"0010_0011",# "pxi":"0100_0000","pxo":"0100_0001","lpxa":"0100_0010","lpxb":"0100_0011","spxa":"0100_0100","spxb":"0100_0101","wrt":"0100_0110","axi":"0100_1000",# "hlt":"1000_0000","sxa":"1000_0010","sxb":"1000_0011","sya":"1000_0100","syb":"1000_0101","sva":"1000_0110","svb":"1000_0111",# "int1":"1000_1000","int2":"1000_1001"
-#int1-keyboard,int2-mouse
-#nop - do nothing
-#ld(a or b) addr - set a or b from the address in ram
-#st(a or b) addr - set the address in ram from a or b
-#not(a or b) - not a or b and set to a or b
-#shl(a or b) - shift a or b left and set to a or b
-#shr(a or b) - shift a or b right and set to a or b
-#lx(a or b) - set the x register with a or b
-#ly(a or b) - set the y register with a or b
-#lv(a or b) - set the v register with a or b
-#inc(a or b or sp) - increment a or b or stack pointer and set to a or b or stack pointer
-#dec(a or b or sp) - decrement a or b or stack pointer and set to a or b or stack pointer
-#psh(a or b) - push a or b onto stack (does not increment the stack pointer)
-#pop(a or b) - take whts on the stack and store into a or b (does not decrement the stack pointer)
-#add, sub, or, xor - does this alu op and stores to a, i.e. for add a+b->a
-#cmp - compares a and b, sets flags
-#ld(a or b)v value - load value into a or b
-#jmp addr - jump to address plus 1
-#thes are jump ops based of flags: jmz - jump if Zero, jnz - jump if not Zero, jmc - jump if CarryOut, jnc - jump if not CarryOut, jme - jump if Equal, jne - jump if not Equal, jma - jump if ALarger, jna - jump if not ALarger
-#pxi - turns on pixel and sets VRAM to 1 at register x,register y (X,Y)
-#pxo - turns off pixel and sets VRAM to 0 at register x,register y (X,Y)
-#wri - puts a character and sets VRAM for pixels of char at ofsets from register x,register y (X,Y). register v is char.
-#int1 addr - wait for key input from user, store to ram at address
-#int2 addr - read mouseX, store to ream at address
-#int3 addr - read mouseY, store to ream at address
-#int4 addr - read mouseLeftButton (0 not pressed, 1 pressed), store to ream at address
-#int6 addr - read mouseRightButton (0 not pressed, 1 pressed), store to ream at address
-#to use a number value do #num; to use single char do 'char; commnet is ";"
-#VRAM size is 21x68, seperate from RAM
-#RAM size is 256*256
-#stack size is 32, seperate from RAM
-#changeable registers - sp, a, b, x, y, v
+
 }
 ltrs={
     "A":33,"B":34,"C":35,"D":36,"E":37,"F":38,"G":39,"H":40,"I":41,"J":42,"K":43,"L":44,"M":45,"N":46,"O":47,"P":48,"Q":49,"R":50,"S":51,"T":52,"U":53,"V":54,"W":55,"X":56,"Y":57,"Z":58,"a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8,"i":9,"j":10,"k":11,"l":12,"m":13,"n":14,"o":15,"p":16,"q":17,"r":18,"s":19,"t":20,"u":21,"v":22,"w":23,"x":24,"y":25,"z":26,"1":64,"2":65,"3":66,"4":67,"5":68,"6":69,"7":70,"8":71,"9":72,"0":73,"~":74,"!":75,"@":76,"#":77,"$":78,"%":79,"^":80,"&":81,"*":82,"(":83,")":84,"_":85,"-":86,"+":87,"=":88,"{":89,"[":90,"}":91,"]":92,"|":93,"\\":94,":":95,";":96,"\"":97,"'":98,"<":99,",":100,">":101,".":102,"?":103,"/":104,"¯":105,"☒":0," ":105,
